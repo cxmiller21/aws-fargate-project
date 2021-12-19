c542c1b7bdd51ed5ec8189f58d50225200edf486
Cooper Miller <cooperxmiller@gmail.com>
2021-12-19 12:54:11 -0600

    frontend vue app running on port 8080

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <NodeCall/>
+</template>
+
+<script>
+import NodeCall from './components/NodeCall';
+export default {
+  name: 'App',
+  components: {
+    NodeCall
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ frontend/src/components/NodeCall.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "NodeCall",
+  props: {},
+  data() {
+    return {
+      msg: "",
+      loading: true,
+      errored: false,
+    };
+  },
+  mounted() {
+    axios
+      .get(`http://localhost:3000/getData`)
+      .then((resp) => resp.data)
+      .then((data) => {
+        this.msg = data.msg;
+      })
+      .catch((error) => {
+        console.log(error);
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
